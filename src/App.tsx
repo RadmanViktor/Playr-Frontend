c4d9a7798d059e13f0be068b7627ffa3d1c84b20
Viktor Rådman <viktor.radman@biometria.se>
2026-08-22 12:11:40 +0200

    feat: route protected pages through AppShell, remove DashboardPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
-import DashboardPage from './pages/DashboardPage'
+import HomePage from './pages/HomePage'
+import FeedPage from './pages/FeedPage'
+import FindPlayersPage from './pages/FindPlayersPage'
+import ThreadsPage from './pages/ThreadsPage'
+import ProfilePage from './pages/ProfilePage'
 import { ProtectedRoute } from './components/ProtectedRoute'
+import { AppShell } from './components/layout/AppShell'
 
 function App() {
   return (
@@ -10,13 +15,18 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route
-        path="/"
         element={
           <ProtectedRoute>
-            <DashboardPage />
+            <AppShell />
           </ProtectedRoute>
         }
-      />
+      >
+        <Route index element={<HomePage />} />
+        <Route path="/feed" element={<FeedPage />} />
+        <Route path="/find-players" element={<FindPlayersPage />} />
+        <Route path="/threads" element={<ThreadsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Route>
     </Routes>
   )
 }
